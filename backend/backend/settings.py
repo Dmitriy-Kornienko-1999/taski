a74ecd63e588d148478af04e3c019e85826bd5f5
root <root@epdh2f6i4323p8qpvqc2.auto.internal>
2023-12-22 12:49:47 +0000

    Completed_Taski_1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.201.162.206', '127.0.0.1', 'localhost', 'taskikda.ddnsking.com']
+ALLOWED_HOSTS = ['51.250.106.233', '127.0.0.1', 'localhost', 'taskikda.ddnsking.com']
 
 
 # Application definition
